8bae8fffc2fc6d47c906820c371e5e6c27b6b83e
trangphamptit <thuytrang.bcvt2015@gmail.com>
2019-06-23 10:12:16 +0700

    create containerpage

---
+++ src/containerpage/ContainerPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+class ContainerPage extends Component {
+  render() {
+    return (
+      <div className="mainpage">
+        <Header />
+        <section>{this.props.children}</section>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+}
+
+export default ContainerPage;
