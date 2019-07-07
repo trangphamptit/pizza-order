a10e310b4b6177a2634b1d52beb25c9d4a4792f6
Minh Phan <pnminh1710@gmail.com>
2019-07-07 18:33:00 +0700

    update

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Product from "../../components/Product/Product";
 import { AppContext } from "../../services/AppContext";
-import { makeStyles } from "@material-ui/core/styles";
-import "./Products.scss";
 import Grid from "@material-ui/core/Grid";
+
+import "./Products.scss";
+
 class Products extends Component {
   componentDidMount() {
     this.context.getProducts();
@@ -11,9 +12,8 @@ class Products extends Component {
 
   render() {
     const { products } = this.context;
-    // console.log(products);
     return (
-      <Grid container className="products">
+      <Grid container spacing={4} className="products">
         {products.map((product, index) => (
           <Product key={index} product={product} />
         ))}
